94414f2eae4c685d449934e4aba550387c3a3b04
liuzilong <liuzilong@local>
2026-07-01 18:16:59 +0800

    chore(installer): exclude build artifacts

--- installer-shell/target/debug/deps/unicode_ident-26a97d5066a2a372.d
+++
@@ -1,8 +0,0 @@
-/Users/liuzilong/Projects/ChangLi/installer-shell/target/debug/deps/unicode_ident-26a97d5066a2a372.d: /Users/liuzilong/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /Users/liuzilong/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
-
-/Users/liuzilong/Projects/ChangLi/installer-shell/target/debug/deps/libunicode_ident-26a97d5066a2a372.rlib: /Users/liuzilong/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /Users/liuzilong/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
-
-/Users/liuzilong/Projects/ChangLi/installer-shell/target/debug/deps/libunicode_ident-26a97d5066a2a372.rmeta: /Users/liuzilong/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /Users/liuzilong/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
-
-/Users/liuzilong/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs:
-/Users/liuzilong/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs:
